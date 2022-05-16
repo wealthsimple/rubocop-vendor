59f71fc29d1a14498716112bccf5475194718f6a
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-05-16 10:44:31 -0400

    stop requiring version bump for non code changes

--- spec/vendor/version_spec.rb
+++ spec/vendor/version_spec.rb
@@ -5,28 +5,69 @@ require 'keepachangelog'
 
 RSpec.describe 'a published gem' do # rubocop:disable RSpec/DescribeClass
   def get_version(git, branch = 'HEAD')
-    git.grep('STRING = ', 'lib/**/version.rb', object: branch)
+    git.grep('VERSION = ', 'lib/*/version.rb', { object: branch })
        .map { |_sha, matches| matches.first[1] }
-       .map(&method(:parse_version))
-       .compact
+       .filter_map { |str| parse_version(str) }
        .first
+  rescue Git::GitExecuteError
+    # Catches failures for branch name being master
+    nil
   end
 
   def parse_version(string)
-    string.match(/STRING = ['"](.*)['"]/)[1]
+    string.match(/VERSION = ['"](.*)['"]/)[1]
+  end
+
+  def main_branch
+    @main_branch ||=
+      if git.branches['origin/main']
+        'origin/main'
+      elsif git.branches['origin/master']
+        'origin/master'
+      else
+        raise StandardError,
+              <<~ERROR
+                Couldn't determine main branch.
+                Does 'origin/main' or 'origin/master' need to be fetched?
+              ERROR
+      end
+  end
+
+  def needs_version_bump?
+    base = git.merge_base(main_branch, 'HEAD').first&.sha
+    base ||= main_branch
+    git.diff(base, 'HEAD').any? do |diff|
+      not_gemfile?(diff) && not_lockfile?(diff) && not_ci_file?(diff)
+    end
+  end
+
+  def not_lockfile?(diff)
+    diff.path != 'Gemfile.lock'
+  end
+
+  def not_gemfile?(diff)
+    diff.path != 'Gemfile'
+  end
+
+  def not_ci_file?(diff)
+    !diff.path.start_with?('.github/')
   end
 
   let(:git) { Git.open('.') }
   let(:head_version) { get_version(git, 'HEAD') }
-  let(:main_version) { get_version(git, 'origin/main') }
 
   it 'has a version number' do
     expect(head_version).not_to be_nil
   end
 
   it 'has a bumped version committed' do
-    is_main_branch = git.current_branch == 'main' || main_version.nil?
+    main_version = get_version(git, main_branch)
+    skip('first time publishing, no need to compare versions') if main_version.nil?
+
+    is_main_branch = git.current_branch == main_branch
     skip('already on main branch, no need to compare versions') if is_main_branch
+
+    skip('Diff only contains non-code changes, no need to bump version') unless needs_version_bump?
 
     expect(Gem::Version.new(head_version)).to be > Gem::Version.new(main_version)
   end
@@ -38,6 +79,6 @@ RSpec.describe 'a published gem' do # rubocop:disable RSpec/DescribeClass
   it 'has changelog entry for current version' do
     parser = Keepachangelog::MarkdownParser.load('CHANGELOG.md')
 
-    expect(parser.parsed_content['versions'].keys).to include(start_with("#{head_version} - "))
+    expect(parser.parsed_content['versions'].keys).to include(start_with(head_version))
   end
 end
