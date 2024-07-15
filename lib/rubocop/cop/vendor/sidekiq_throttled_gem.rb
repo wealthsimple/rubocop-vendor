e703a0aa73f80320180309ee1ed894029c8aa500
Tyler Marien <tmarien@wealthsimple.com>
2024-07-15 13:18:19 -0600

    chore: remove slack references
    We are implementing a Slack retention policy, so these Slack messages are going to
    be removed. Removing references to Slack to prevent confusion in the future. I
    think that all the information needed is already contained in the comment, so we
    should be able to just remove the references.

--- lib/rubocop/cop/vendor/sidekiq_throttled_gem.rb
+++ lib/rubocop/cop/vendor/sidekiq_throttled_gem.rb
@@ -9,8 +9,6 @@ module RuboCop
       # resulting in at-most once delivery of sidekiq jobs, instead of at-least once delivery
       # of sidekiq jobs. This means there is a relatively good chance you will LOSE JOBS that were enqueued
       # if the sidekiq-process runs out of memory or does not shutdown gracefully.
-      #
-      # https://wealthsimple.slack.com/archives/C19UB3HNZ/p1683721247371709 for more details
       class SidekiqThrottledGem < Base
         MSG = <<~MSG.strip
           Do not use the sidekiq-throttled gem. sidekiq-throttled overrides the fetcher of sidekiq-pro and sidekiq-enterprise resulting in the ability to lose jobs (switch to at-most once processing).
